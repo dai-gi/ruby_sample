f325fdc7c0a44963b28652387b79daf3b60e40ec
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-01-17 23:10:16 +0900

    Improve: プログラムをメソッド化

--- sample.rb
+++ sample.rb
@@ -1,12 +1,16 @@
-num = 0
-while num <= 10
-  point = gets.to_i
-  if point < 1
-    puts "エラーです"
-  elsif point > 5
-    puts "エラーです"
-  else
-    puts "ありがとうございます"
+def input_point
+  num = 0
+  while num <= 10
+    point = gets.to_i
+    if point < 1
+      puts "エラーです"
+    elsif point > 5
+      puts "エラーです"
+    else
+      puts "ありがとうございます"
+    end
+    num = num + 1
   end
-  num = num + 1
 end
+
+input_point
