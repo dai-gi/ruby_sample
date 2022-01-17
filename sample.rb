39351d20bc835d0e2b51bc7e8eddc1c9b9cbf5b2
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-01-17 22:50:13 +0900

    Improve: 入力を10回繰り返すように改善

--- sample.rb
+++ sample.rb
@@ -1,8 +1,12 @@
-point = gets.to_i
-if point < 1
-  puts "エラーです"
-elsif point > 5
-  puts "エラーです"
-else
-  puts "ありがとうございます"
+num = 0
+while num <= 10
+  point = gets.to_i
+  if point < 1
+    puts "エラーです"
+  elsif point > 5
+    puts "エラーです"
+  else
+    puts "ありがとうございます"
+  end
+  num = num + 1
 end
