cc42b46e3943a4908ba4b2038fc9eefef654515b
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-01-19 23:57:08 +0900

    Add: 評価ポイントとコメントを受け取って表示するプログラムを作成中

--- sample.rb
+++ sample.rb
@@ -1,16 +1,23 @@
-def input_point
-  num = 0
-  while num <= 10
-    point = gets.to_i
-    if point < 1
-      puts "エラーです"
-    elsif point > 5
-      puts "エラーです"
-    else
-      puts "ありがとうございます"
+puts "1から5の数字で評価を入力してください。終了する場合は「6」を入力してください"
+val = gets.to_i
+
+def validation(arg)
+  while true
+    if arg.class != arg.to_i.class
+      puts "数字を入力してください"
     end
-    num = num + 1
-  end
 end
 
-input_point
+count = 0
+while count <= 2
+  while count
+    validation(val)
+  end
+
+  if val == 6
+    puts "終了します"
+    exit
+  else
+    puts "あなたのポイントは：#{val}あなたのコメント：○○○"
+  end
+end
