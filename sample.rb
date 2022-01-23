ffb4416d61a9b9edab59298f2438b31ec950dc7e
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-01-23 23:08:48 +0900

    Improve: 条件分岐の設定方法を改善
    if = 狭い条件を設定
    elsif = ifの条件からは漏れたけれども他の条件に当てはまるものを設定
    else = それ以外という広い条件を設定
    
    指定する条件が狭いものからif、elsif、else
    の順に設定

--- sample.rb
+++ sample.rb
@@ -3,13 +3,16 @@ while true
   puts "1から5の数字で評価を入力してください。終了する場合は「6」を入力してください"
   point = gets.to_i
 
-  if point == 0
-    puts "数字を入力してください"
-  elsif point == 6
+  if point == 6
     puts "終了します"
-    exit
-  else
+    break
+
+  elsif point != 0
+    puts "コメントを入力してください"
     comment = gets
-    puts "あなたのポイント：#{point} あなたのコメント：#{comment}"
+    puts "あなたのポイント: #{point}"
+    puts "あなたのコメント: #{comment}"
+  else
+    puts "数字を入力してください"
   end
 end
