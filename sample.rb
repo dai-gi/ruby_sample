bccb0da9193976723529913aee061caee15520a8
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-02-06 19:20:04 +0900

    Improve: 機能を大きく3に分別 1：投稿を記録 2：今までの履歴を表示 3：終了

--- sample.rb
+++ sample.rb
@@ -1,23 +1,35 @@
-posts = []
+def record(point, comment)
+  post = "ポイント: #{point} コメント: #{comment}"
+  file = File.open("data.txt", "a")
+  file.puts(post)
+  file.close
+end
+
+def history
+  read_file = File.open("data.txt", "r")
+  puts read_file.read
+  read_file.close
+end
 
 while true
-  puts "1から5の数字で評価を入力してください。終了する場合は「6」を入力してください"
-  point = gets.to_i
+  puts "1:評価ポイントとコメントを入力する"
+  puts "2:今までの結果を確認する"
+  puts "3:終了する"
 
-  if point == 6
-    puts "終了します"
-    break
-
-  elsif point != 0
+  num = gets.to_i
+  case num
+  when 1
+    puts "1から5の数字で評価を入力してください。終了する場合は「6」を入力してください"
+    point = gets.to_i
     puts "コメントを入力してください"
     comment = gets
-    post = "ポイント: #{point} コメント: #{comment}"
-    file = File.open("data.txt", "a")
-    file.puts(post)
-    file.close
-    read_file = File.open("data.txt", "r")
-    puts read_file.read
-    read_file.close
+    record(point, comment)
+  when 2
+    puts "これまでの結果"
+    history()
+  when 3
+    puts "終了します"
+    break
   else
     puts "数字を入力してください"
   end
