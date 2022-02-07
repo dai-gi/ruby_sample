c654e3cadbbab1b9a627b19248a512fc42a3e81a
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-02-07 19:48:16 +0900

    Improve: 記録と閲覧の処理を関数だけで完結するように改善

--- sample.rb
+++ sample.rb
@@ -1,11 +1,21 @@
-def record(point, comment)
-  post = "ポイント: #{point} コメント: #{comment}"
-  File.open("data.txt", "a") do |file|
-    file.puts(post)
+def record()
+  puts "1から5の数字で評価を入力してください"
+  point = gets.to_i
+  if point <= 0 || point > 5
+    puts "1から5を入力してください"
+    record()
+  else
+    puts "コメントを入力してください"
+    comment = gets
+    post = "ポイント: #{point} コメント: #{comment}"
+    File.open("data.txt", "a") do |file|
+      file.puts(post)
+    end
   end
 end
 
 def history
+  puts "これまでの結果"
   File.open("data.txt", "r") do |file|
     file.each_line do |each_line|
       puts each_line
@@ -22,17 +32,8 @@ while true
   num = gets.to_i
   case num
   when 1
-    puts "1から5の数字で評価を入力してください"
-    point = gets.to_i
-    if point <= 0 || point > 5
-      puts "1から5を入力してください"
-    else
-      puts "コメントを入力してください"
-      comment = gets
-      record(point, comment)
-    end
+    record()
   when 2
-    puts "これまでの結果"
     history()
   when 3
     puts "終了します"
