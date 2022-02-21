fa9c1099252fbfa84ac1c634038610ae2a360b5b
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-02-21 21:14:07 +0900

    Improve: shopping_endメソッドの処理を設定

--- procedure_1.rb
+++ procedure_1.rb
@@ -7,6 +7,7 @@ class Grocery
   end
 end
 
+
 # 購入者クラスの設定
 class Customer
   attr_accessor :name, :address
@@ -19,6 +20,20 @@ class Customer
 
   def shopping(groceries)
     # 商品リストに基づき、ショッピングの処理を行う
+    # 買い物を開始する.
+    grocery_list(groceries)
+    shopping_end = nil
+    while shopping_end != "yes" do
+      # 商品選択
+      puts "商品番号選択"
+      number = gets.to_i
+      # 数入力
+      puts "商品数量入力"
+      quantity = gets.to_i
+      entry(groceries[number], quantity)
+      puts "買い物終了するか？ yes/no"
+      shopping_end = gets.chomp
+    end
     # @basket を買い物かごとして使用する
+    shopping_list(@basket)
   end
-end
